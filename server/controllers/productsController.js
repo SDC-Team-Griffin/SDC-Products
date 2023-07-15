36275cc1cc447ab1425c6f4a999512f0217d82e0
darienpoon <darienpoon1@gmail.com>
2023-07-14 18:10:55 -0700

    cleaned up response data from db to only pass on relevant data

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -5,8 +5,9 @@ module.exports = {
   getProducts: ( function (req, res) {
     productsModel.loadProducts()
     .then((products) => {
-      console.log("products", products);
-      res.status(200).send(products)
+      var productRows = products.rows;
+      console.log("products", productRows);
+      res.status(200).send(productRows)
     })
     .catch((err) => {
       console.error('Error loading products:', err)
@@ -17,8 +18,9 @@ module.exports = {
   getDefaultProduct : ( function (req, res) {
     productsModel.loadDefaultProduct()
     .then((defaultProduct) => {
-      console.log('defaultProduct', defaultProduct);
-      res.status(200).send(defaultProduct)
+      var defaultProductRows = defaultProduct.rows;
+      console.log('defaultProduct', defaultProductRows);
+      res.status(200).send(defaultProductRows)
     })
     .catch((err) => {
       console.error('Error loading default product:', err)
@@ -30,8 +32,9 @@ module.exports = {
     var product_id = req.params.product_id
     productsModel.loadSpecificProduct(product_id)
     .then((specificProduct) => {
-      console.log('specificProduct', specificProduct);
-      res.status(200).send(specificProduct)
+      var specificProductRows = specificProduct.rows;
+      console.log('specificProduct', specificProductRows);
+      res.status(200).send(specificProductRows)
     })
     .catch((err) => {
       console.error('Error loading specific product', err)
@@ -43,8 +46,9 @@ module.exports = {
     var product_id = req.params.product_id
     productsModel.loadProductStyles(product_id)
     .then((productStyles) => {
-      console.log('product styles', productStyles);
-      res.status(200).send(productStyles)
+      var productStylesRows = productStyles.rows;
+      console.log('product styles', productStylesRows);
+      res.status(200).send(productStylesRows)
     })
     .catch((err) => {
       console.error('Error loading product styles', err)
@@ -56,8 +60,9 @@ module.exports = {
     var product_id = req.params.product_id
     productsModel.loadRelatedProducts(product_id)
     .then((relatedProducts) => {
-      console.log('related products', relatedProducts);
-      res.status(200).send(relatedProducts)
+      var relatedProductRows = relatedProducts.rows;
+      console.log('related products', relatedProductRows);
+      res.status(200).send(relatedProductRows)
     })
     .catch((err) => {
       console.error('Error loading related products', err)
--- server/routes.js
+++ server/routes.js
@@ -11,3 +11,8 @@ router.get('/products/:product_id', productsController.getProductById);
 
 
 module.exports = router;
+
+// the ROUTE PATH: ROUTE URL, controller method function (that calls model methods(database commmand queries)
+// serves purpose of route (router.get/'path', controller.get)
+// where controller.get is function that calls models.get, which is methods of database
+// in the controller is where we pass the req.body/user input, set params, and pass them as arguments of the model database method, then invoke model method
