641ad1fef509a3fa9e5ef3f36bc65ecf4389d461
darienpoon <darienpoon1@gmail.com>
2023-07-14 21:05:46 -0700

    updated query commands to structure responses

--- server/models/productsModel.js
+++ server/models/productsModel.js
@@ -15,22 +15,46 @@ module.exports = {
   loadSpecificProduct: (product_id) => { // SELECT * FROM products features WHERE product_id = ${product_id}`
     console.log('got into get specific product');
     return db.query(`
-      SELECT p.*, f.*
+      SELECT
+      p.*,
+      json_agg(json_build_object(
+        'feature_id', f.feature_id,
+        'feature', f.feature,
+        'value', f.value
+      ) ORDER BY f.feature_id) AS features
       FROM products p
       JOIN features f ON p.product_id = f.product_id
       WHERE p.product_id = ${product_id}
+      GROUP BY p.product_id
     `)
   },
 
-  loadProductStyles: (product_id) => { // SELECT * FROM styles WHERE product_id = ${product_id}
+  loadProductStyles: (product_id) => {
     console.log('got into get product styles');
     return db.query(`
-      SELECT st.*, sk.*,ph.*
+      SELECT
+      st.*,
+      (
+        SELECT json_agg(json_build_object(
+          'photo_id', ph.photo_id,
+          'url', ph.url,
+          'thumbnail_url', ph.thumbnail_url
+        ))
+        FROM photos ph
+        WHERE ph.style_id = st.style_id
+      ) AS photos,
+      (
+        SELECT json_agg(json_build_object(
+          'sku_id', sk.sku_id,
+          'size', sk.size,
+          'quantity', sk.quantity
+        ))
+        FROM skus sk
+        WHERE sk.style_id = st.style_id
+      ) AS skus
       FROM styles st
-      JOIN skus sk ON st.style_id = sk.style_id
-      JOIN photos ph ON st.style_id = ph.style_id
       WHERE st.product_id = ${product_id}
-    `)
+    `);
   },
 
   loadRelatedProducts: (product_id) => {
